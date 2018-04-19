fbb389c594f0458335a7900ec47633f6fcda20ed
Shreyas Joshi <30567439+ShreyasJoshi7@users.noreply.github.com>
2018-04-19 12:42:40 +0530

    Update __init__.py

--- roadfighter/__init__.py
+++ roadfighter/__init__.py
@@ -1,6 +1,10 @@
-from gym.envs.registration import register
+
+from gym.envs.registration import registry, register, make, spec
 
 register(
     id='roadfighter-v0',
     entry_point='gym_foo.envs:RoadfighterEnv',
+    max_episode_steps=1000,
+    reward_threshold=200,
+    
 )
